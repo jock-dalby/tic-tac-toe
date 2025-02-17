e22625ad7081ebbb83573475a190fec459ed87ba
Jock Dalby <jockdalby86@gmail.com>
2025-02-17 10:20:39 +1000

    assign playerName span/input to a variable

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,10 +7,16 @@ function Player({ name, symbol }) {
         setIsEditing(!isEditing);
     }
 
+    let playerName = <span className="player-name">{ name }</span>
+
+    if (isEditing) {
+        playerName = <input  type="text" required/>
+    }
+
     return (
         <li>
             <span className="player">
-                {isEditing ? <input /> : <span className="player-name">{ name }</span>}
+                {playerName}
                 <span className="player-symbol">{ symbol }</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
