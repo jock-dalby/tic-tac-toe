ff8a3b5a225fa5ab1115dd1c62c80225af8b7339
Jock Dalby <jockdalby86@gmail.com>
2025-02-24 08:34:02 +1000

    configure handleSelectSquare function in GameBoard component

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const INITIAL_GAME_BOARD = [
   ["X", null, null],
   [null, null, null],
@@ -5,14 +7,28 @@ const INITIAL_GAME_BOARD = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(INITIAL_GAME_BOARD);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {INITIAL_GAME_BOARD.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((cellValue, colIndex) => (
               <li key={colIndex}>
-                <button>{cellValue}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {cellValue}
+                </button>
               </li>
             ))}
           </ol>
